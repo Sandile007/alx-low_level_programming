8415100a9dc9fc2f62434c24e882885fb3e65303
Sandile <chynisemczee@gmail.com>
2023-11-01 00:45:41 -0700

    Static Libraries C

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
 
 int _putchar(char c);
@@ -23,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif /* main_h */
